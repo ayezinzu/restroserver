070b3c4eb605fa2118e916cbd863e58d0fdd1aa5
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2020-09-16 14:22:19 +0530

    fix

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -4,6 +4,7 @@ const restaurantController = require('../controller/restaurant')
 router.post('/restaurantSignin', restaurantController.singinRestaurant_post)
 router.post('/restaurantSignup', restaurantController.singupRestaurant_post)
 router.post('/colorPost', restaurantController.color_post)
-router.get('/colorGet', restaurantController.color_get)
+router.post('/colorGet', restaurantController.color_get)
+
 
 module.exports = router
